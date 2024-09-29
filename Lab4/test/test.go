2915fb5a1119379790187b341158f093b7d57b82
Litusuwu <zegarraward@gmail.com>
2024-09-28 20:04:27 -0500

    testing go

---
+++ Lab4/test/test.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main(){
+	var str string
+	var n, m int
+	var mn float32
+
+	str = "Hello world!"
+	n = 10
+	m = 20
+	mn = 2.45
+
+	fmt.Println("Valor de str : ", str)
+	fmt.Println("Valor de n : ", n)
+	fmt.Println("Valor de m : ", m)
+	fmt.Println("Valor de mn : ", mn)
+
+	var ciudad string = "Loksea"
+	var x int = 238
+	
+	fmt.Println("El valor de ciudad es : ", ciudad)
+	fmt.Println("El valor de x es : ", x>)
+
+	cadena := "cadenita"
+	numerin := 12345
+	numerinDecimal := 123.123
+
+	fmt.Println("El valor de cadenita es : ", cadena)
+	fmt.Println("El valor de numemrin es : ", numerin )
+	fmt.Println("El valor de numerinDecimal es : ", numerinDecimal)
+	// fmt.Print(type(cadenita))
+	var(
+		nombre string
+		apellido string
+		correo string
+	)
+	nombre = "Litus"
+	apellido = "Zegarra"
+	correo = "carlos.zegarrab@pucp.edu.pe"
+	fmt.Println("Mi nombre es",nombre,apellido,"y mi correo es :", correo)
+}
