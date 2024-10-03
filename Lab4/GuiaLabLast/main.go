2f2e7b626438f8ae21edbae7d6bd64f236d68d83
Litusuwu <carlos.zegarrab@pucp.edu.pe>
2024-10-03 11:24:09 -0500

    guialab

--- Lab4/GuiaLabLast/main.go
+++ Lab4/GuiaLabLast/main.go
@@ -8,22 +8,30 @@ import (
 var (
 	buffer [5]int = [5]int{-1, -1, -1, -1, -1}
 	index int
-	controlConsumidor chan bool = make(chan bool, 5)
-	controlProductor chan bool = make(chan bool, 5)
+	counter int = 0
+	// controlConsumidor chan bool = make(chan bool, 5)
+	vacio chan bool = make(chan bool, 1)
+	lleno chan bool = make(chan bool, 1)
 	wg sync.WaitGroup
 	mutex sync.Mutex
 )
 
 func productor() {
 	for n := 0; n < 20; n++ {
+		mutex.Lock()
 		item := n * n
 		index = n % 5
-		controlProductor <- true
-		mutex.Lock()
 		buffer[index] = item
 		fmt.Printf("Productor %d %d %v\n", index, item, buffer)
+		counter++
+		if(counter == 5){
+			lleno <- true
+		}
 		mutex.Unlock()
-		controlConsumidor <- true
+		if(counter == 5){
+			<-vacio // esperar a que se vacie
+		}
+		// controlConsumidor <- true
 	}
 	wg.Done()
 }
@@ -31,21 +39,29 @@ func productor() {
 func consumidor() {
 	var item int
 	for n := 0; n < 20; n++ {
-		index = n % 5
-		<- controlConsumidor
+		if(counter == 0){
+			<-lleno
+		}
 		mutex.Lock()
+		index = n % 5
+		// <- controlConsumidor
 		item = buffer[index]
 		buffer[index] = -1
 		fmt.Printf("Consumidor %d %d %v\n", index, item, buffer)
+		counter--
 		mutex.Unlock()
-		<- controlProductor
+		if(counter == 0){
+			vacio <- true
+		}
 	}
 	wg.Done()
 }
 
 func main() {
 	wg.Add(2)
-	go consumidor()
+	// controlProductor<-true
 	go productor()
+	go consumidor()
+	
 	wg.Wait()
 }
